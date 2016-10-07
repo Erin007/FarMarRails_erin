be6455e1d103501ad0cfa793351a31826d65ea6b
Erin Bond <erin@erinebond.com>
2016-10-06 22:40:51 -0700

    Styling for market show page

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -51,13 +51,13 @@ class SalesController < ApplicationController
     end
 
     if @sale_of_interest == nil
-      @sale_of_interest = { id: params[:id].to_i, amount: "That sale was not found", purchse_time: "", vendor_id: "", product_id: ""}
+      @sale_of_interest = { id: params[:id].to_i, amount: "That sale was not found", purchase: "", vendor_id: "", product_id: ""}
     end
   end
 
 private
   def sale_params
      #Tells Rails which parameters can be changed
-     params.require(:sale).permit(:id, :amount, :purchse_time, :vendor_id, :product_id)
+     params.require(:sale).permit(:id, :amount, :purchase, :vendor_id, :product_id)
    end
 end
